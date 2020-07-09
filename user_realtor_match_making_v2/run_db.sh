722cf3961c36bacfa0b335b16c89bf67ad6d5ba0
Jatin Malhotra <35250709+jmalhot@users.noreply.github.com>
2020-07-08 21:48:45 -0400

    Add files via upload

---
+++ user_realtor_match_making_v2/run_db.sh
@@ -0,0 +1,1 @@
+./cloud_sql_proxy -instances='nestready-platform-production:us-central1:api-production=tcp:5434' -credential_file='/home/jatin/nestready-platform-production-sa-ml-cloud-sql-client-f809c1b2cb09.json'
