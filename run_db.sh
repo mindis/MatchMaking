20be8c61ac28337409b9147545455b645c4a531d
Jatin Malhotra <35250709+jmalhot@users.noreply.github.com>
2020-09-16 09:51:09 -0400

    Update run_db.sh

--- run_db.sh
+++ run_db.sh
@@ -1,1 +1,1 @@
-./cloud_sql_proxy -instances='nestready-platform-production:us-central1:api-production=tcp:PORT' -credential_file='/home/jatin/nestready-platform-production-sa-ml-cloud-sql-client-f809c1b2cb09.json'
+./cloud_sql_proxy -instances='<system_name>:us-central1:api-production=tcp:PORT' -credential_file='/home/jatin/<system_name>-sa-ml-cloud-sql-client-f809c1b2cb09.json'
